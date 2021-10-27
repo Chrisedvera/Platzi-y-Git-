d9b76b76b9048cd82dfca6974f31fec6ef2b30b5
Christian <christian.suarez9527@gmail.com>
2021-10-27 13:57:07 -0500

    Se agragron condicionales

--- conversor.py
+++ conversor.py
@@ -1,4 +1,29 @@
-pesos = float(input("¿Cuantos pesos colombianos tienes?: "))
-valor_dolar = 3750
-dolares = pesos / valor_dolar
-print(" Tiene " + str(dolares) + " USD")
+menu = """ Bienvenido al conversor de moneada.
+las opciones son :
+
+1 - ARS
+2 - COP
+3 - MXN
+
+Elige una de las opciones: 
+"""
+opcion = int(input(menu))
+
+
+if opcion == 1:
+    pesos = float(input("¿Cuantos pesos argentinos tienes?: "))
+    valor_dolar = 65
+    dolares = pesos / valor_dolar
+    print(" Tiene " + str(dolares) + " USD")
+elif opcion == 2:
+    pesos = float(input("¿Cuantos pesos mexicnaos tienes?: "))
+    valor_dolar = 25
+    dolares = pesos / valor_dolar
+    print(" Tiene " + str(dolares) + " USD")
+elif opcion == 3:
+    pesos = float(input("¿Cuantos pesos colombianos tienes?: "))
+    valor_dolar = 3750
+    dolares = pesos / valor_dolar
+    print(" Tiene " + str(dolares) + " USD")
+else:
+    print("Por favor eleige una de las opciones")
